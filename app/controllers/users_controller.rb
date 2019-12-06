bb7876ca031e9c5c847a9aee8c65deeb9ac526df
Santiago Guerra <guerrasantiago19@gmail.com>
2019-12-06 16:57:36 -0500

    html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render 'not working'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
