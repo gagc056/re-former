e4d8d44d24250d46b2321f9742bd69c483545871
Santiago Guerra <guerrasantiago19@gmail.com>
2019-12-06 17:48:54 -0500

    edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def user_params
